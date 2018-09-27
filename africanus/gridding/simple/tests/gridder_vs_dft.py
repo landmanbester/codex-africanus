b66094daf06ca70754ad785c61ff3f3d2e07f218
landmanbester <lbester@ska.ac.za>
2018-09-27 12:37:16 +0200

    Added comparison of gridder with DFT

--- africanus/gridding/simple/tests/gridder_vs_dft.py
+++ africanus/gridding/simple/tests/gridder_vs_dft.py
@@ -53,8 +53,10 @@ def test_forward_operator():
     lm = np.vstack((ll.flatten(), mm.flatten())).T
 
     # create random image and flatten for use with dft
-    image = np.abs(np.random.randn(npix, npix, nchan))  # np.zeros((npix, npix, nchan), dtype=np.float64)
-    # image[npix//2, npix//2, :] = 1.0
+    #image = np.abs(np.random.randn(npix, npix, nchan))  # np.zeros((npix, npix, nchan), dtype=np.float64)
+    image = np.exp(-(ll**2 + mm**2)*100000)[:, :, None]
+    #image = np.zeros((npix, npix, nchan), dtype=np.float64)
+    #image[npix//4, npix//4, :] = 1.0
     image_flat = image.reshape(npix**2, nchan)
 
     # set up dask arrays
@@ -76,7 +78,7 @@ def test_forward_operator():
 
     # get taper
     K = 3*npix
-    full_sup = 21
+    full_sup = 51
     half_sup = (full_sup - 2)//2  # this weird convention needs to die!
     nshift = (np.arange(npix) - (npix-1)//2)/K
     beta = 2.34*full_sup
@@ -88,7 +90,7 @@ def test_forward_operator():
 
     print(image_padded.shape, K)
 
-    osamp = 63
+    osamp = 103
     conv_filter = convolution_filter(half_sup, osamp, "kaiser-bessel")
 
     # get FFT
@@ -113,7 +115,90 @@ def test_forward_operator():
 
     return
 
+def test_backward_operator():
+    da = pytest.importorskip("dask.array")
+    from africanus.dft.dask import vis_to_im
+    from africanus.filters import convolution_filter
+    from africanus.filters import kaiser_bessel_filter as kbf
+    from africanus.gridding.simple import grid
 
+    # start by simulating some uv-coverage
+    nrow = 1000
+    uvw = (0.1 + np.random.random(size=(nrow, 3)))*128
+    uvw[:, -1] = 0.0  # set w to zero for simple gridder
+
+    nchan = 1 # single channel test since gridder averages channels
+    frequency = np.array([1e9], dtype=np.float64)
+    wavelengths = lightspeed/frequency
+    npix = 129
+    cell_size = estimate_cell_size(uvw[:, 0], uvw[:, 1],
+                                   wavelengths, factor=5, nx=npix, ny=npix).min()
+
+    cor = (1,)
+    ncor = 1
+    weights = np.ones((nrow, nchan) + cor, dtype=np.float64)
+    vis = np.random.randn(nrow, nchan) + 1.0j*np.random.randn(nrow, nchan)
+
+    cell_size_rad = cell_size * _ARCSEC2RAD
+
+    nover2 = npix//2
+    x = np.linspace(-nover2*cell_size_rad, nover2*cell_size_rad, npix)
+    ll, mm = np.meshgrid(x, x)
+    lm = np.vstack((ll.flatten(), mm.flatten())).T
+
+    # set up dask arrays
+    uvw_dask = da.from_array(uvw, chunks=(250, 3))
+    lm_dask = da.from_array(lm, chunks=(npix**2, 2))
+    frequency_dask = da.from_array(frequency, chunks=1)
+    vis_dask = da.from_array(vis, chunks=(250, 1))
+
+    print("Doing dft")
+    dirty_dft = vis_to_im(vis_dask, uvw_dask, lm_dask, frequency_dask).compute()
+
+    dirty_dft = dirty_dft.reshape(npix, npix)
+
+    print("Getting taper")
+    # get taper
+    K = 3*npix
+    full_sup = 17
+    half_sup = (full_sup - 2)//2  # this weird convention needs to die!
+    nshift = (np.arange(npix) - (npix-1)//2)/K
+    beta = 2.34*full_sup
+    S = kbf.kaiser_bessel_fourier(nshift, full_sup, beta)
+    S = S[:, None] * S[None, :]  # outer product to get 2D version
+
+    # grid the data
+    vis = vis[:, :, None]
+    flags = np.zeros_like(vis, dtype=np.int8)
+    osamp = 63
+    conv_filter = convolution_filter(half_sup, osamp, "kaiser-bessel")
+    dirty = grid(vis, uvw, flags, weights, wavelengths, conv_filter, cell_size, ny=K, nx=K)
+
+    print(dirty.shape)
+
+    # get FFT
+    for ch in range(nchan):
+        dirty[:, :, ch] = Fs(iFFT(iFs(dirty[:, :, ch])))*K**2  # K**2 because we shouldn't normalise
+                                                               # the FFT for comparison with DFT
+
+    # remove zero padding and divide by taper
+    dirty = dirty[npix:-npix, npix:-npix, 0]/S
+
+    plt.figure('dft')
+    plt.imshow(dirty_dft)
+    plt.colorbar()
+
+
+    plt.figure('grid')
+    plt.imshow(dirty.real)
+    plt.colorbar()
+
+    plt.figure('diff')
+    plt.imshow((dirty_dft - dirty.real)/np.abs(dirty_dft).max())
+    plt.colorbar()
+
+    plt.show()
 
 if __name__=="__main__":
-    test_forward_operator()
+    #test_forward_operator()
+    test_backward_operator()
