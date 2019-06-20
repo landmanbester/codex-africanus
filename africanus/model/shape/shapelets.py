Verified 1D shapelet. Modified basis to have correct normalisation when not using anular freq

africanus/model/shape/shapelets.py
@@ -26,10 +26,19 @@ def factorial(n):
     return ans * n
 
 #@numba.jit(nogil=True, nopython=True, cache=True)
-def basis_function(n, xx, beta):
-    basis_component = ((2**n) * ((np.pi)**(0.5)) * factorial(n) * beta)**(-0.5)
-    exponential_component = hermite(n, xx / beta) * np.exp((-0.5) * (xx**2) * (beta **(-2)))
-    return basis_component * exponential_component
+def basis_function(n, xx, beta, fourier=False, delta_x=None):
+    if fourier:
+        x = 2*np.pi*xx
+        scale = 1.0/beta
+    else:
+        x = xx
+        scale = beta
+    basis_component = 1.0/np.sqrt(2.0**n * np.sqrt(np.pi) * factorial(n) * scale)
+    exponential_component = hermite(n, x / scale) * np.exp(-x**2 / (2.0*scale**2))
+    if fourier:
+        return 1.0j**n * basis_component * exponential_component * np.sqrt(2*np.pi)/delta_x
+    else:
+        return basis_component * exponential_component
 
 
 #@numba.jit(nogil=True, nopython=True, cache=True)
@@ -66,3 +75,42 @@ def shapelet(coords, frequency, coeffs_l, coeffs_m, beta, dtype=np.complex128):
                         tmp_shapelet += complex_part * real_part
                 out_shapelets[row, chan, src] = tmp_shapelet
     return out_shapelets
+
+
+def shapelet_1d(u, coeffs, fourier, delta_x=None, beta=1.0):
+    """
+    The one dimensional shapelet. Default is to return the
+    dimensionless version. 
+
+    Parameters
+    ----------
+    u : :class:`numpy.ndarray`
+        Array of coordinates at which to evaluate the shapelet
+        of shape (nrow)
+    coeffs : :class:`numpy.ndarray`
+        Array of shapelet coefficients of chape (ncoeff)
+    fourier : bool
+        Whether to evaluate the shapelet in Fourier space
+        or in signal space
+    beta : float, optional
+        The scale parameter for the shapelet. If fourier is
+        true the scale is 1/beta
+
+    Returns
+    -------
+    out : :class:`numpy.ndarray`
+        The shapelet evaluated at u of shape (nrow)
+    """
+    nrow = u.size
+    if fourier:
+        if delta_x is None:
+            raise(ValueError, "You have to pass in a value for delta_x in Fourier mode")
+        out = np.zeros(nrow, dtype=np.complex128)
+    else:
+        out = np.zeros(nrow, dtype=np.float64)
+    for row, ui in enumerate(u):
+        for n, c in enumerate(coeffs):
+            out[row] += c * basis_function(n, ui, beta, fourier=fourier, delta_x=delta_x)
+    return out 
+
+        

africanus/model/shape/tests/test_shapelets.py
@@ -6,13 +6,16 @@ from scipy.special import factorial, hermite
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-from africanus.model.shape import shapelet, basis_function
+from africanus.model.shape import shapelet, basis_function, shapelet_1d
 from africanus.constants import c as lightspeed
 
 Fs = np.fft.fftshift
 iFs = np.fft.ifftshift
-fft = lambda x:np.fft.fft2(x, norm='ortho')
-ifft = lambda x:np.fft.ifft2(x, norm='ortho')
+from scipy import fftpack
+fft = fftpack.fft
+ifft = fftpack.ifft  
+fft2 = fftpack.fft2
+ifft2 = fftpack.ifft2
 
 def test_image_space():
         # Define all respective values for nrow, ncoeff, etc
@@ -263,6 +266,41 @@ def test_fourier_space_shapelets():
     print(" Max diff = ", np.abs(fft_shapelet - uv_space_shapelet).max())
 
 
+def test_1d_shapelet():
+    # set signal space coords
+    beta = 1.0
+    npix = 513
+    coeffs = np.ones(1, dtype=np.float64)
+    l_min = -15.0 * beta
+    l_max = 15.0 * beta
+    delta_l = (l_max - l_min)/(npix-1)
+    if npix%2:
+        l = l_min + np.arange(npix) * delta_l
+    else:
+        l = l_min + np.arange(-0.5, npix-0.5) * delta_l
+    img_shape = shapelet_1d(l, coeffs, False, beta=beta)
+
+    # get Fourier space coords and take fft
+    u = Fs(np.fft.fftfreq(npix, d=delta_l))
+    fft_shape = Fs(fft(iFs(img_shape)))
+    fft_shape_max = fft_shape.real.max()
+    
+    # get uv space
+    uv_shape = shapelet_1d(u, coeffs, True, delta_x=delta_l, beta=beta)
+    uv_shape_max = uv_shape.real.max()
+
+    print('ratio = ', uv_shape_max/fft_shape_max)
+
+    plt.figure('uv')
+    plt.plot(uv_shape.real, 'k')
+
+    plt.figure('fft')
+    plt.plot(fft_shape.real, 'k')
+
+    plt.figure('diff')
+    plt.plot(uv_shape.real - fft_shape.real, 'k')
+
+    plt.show()
 
 
 
@@ -449,4 +487,6 @@ def _test_single_shapelet():
     plt.close()
     assert np.allclose(np.abs(gf_shapelets), np.abs(codex_shapelets))
 
-test_fourier_space_shapelets()
+# test_fourier_space_shapelets()
+
+test_1d_shapelet()